05aaacfebcb02d8502bb76971d7ff5d08364b440
Leonel Ubeda <leonelubeda@hotmail.com>
2020-10-30 16:47:42 -0600

    refactor: session.js

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -2,15 +2,11 @@ import jwt from 'jsonwebtoken'
 
 const deserializeSession = async (req, res, next) => {
     const authHeader = req.headers['authorization']
-    console.log('pasandooo')
     if(authHeader){
         try{
             const token = authHeader.split(' ')[1]
-            const data = await jwt.verify(token, process.env.SUPER_SECRET)
-            req.user = data
-        }catch (e){
-            // console.log(e)
-        }
+            req.user = await jwt.verify(token, process.env.SUPER_SECRET)
+        }catch { }
 
     }
     next()
