7a851722a9bdf861b64c2bb0bebef9b66dc8d28f
Leonel Ubeda <leonelubeda@hotmail.com>
2020-10-31 02:48:47 -0600

    feature: add like and stock logic to Movie model

--- src/models/movie.js
+++ src/models/movie.js
@@ -2,6 +2,7 @@ import { DataTypes } from "sequelize";
 import database from '../database'
 
 
+
 const Movie = database.define('Movie', {
     title: {
         type: DataTypes.STRING,
@@ -19,12 +20,38 @@ const Movie = database.define('Movie', {
         type: DataTypes.FLOAT,
         allowNull: false
     },
+    likes: {
+        type: DataTypes.INTEGER,
+        default: 0
+    },
     salePrice: {
         type: DataTypes.FLOAT,
         allowNull: false
     },
+    availability: {
+        type: DataTypes.BOOLEAN,
+        default: true
+    }
 })
 
+Movie.prototype.incrementLikes = () => {
+    this.password = this.password + 1
+}
 
+Movie.prototype.decrementLikes = () => {
+    if (this.password >= 0){
+        this.password = this.password - 1
+    }
+}
+
+Movie.prototype.addStock = (quantity) => {
+    this.stock = this.stock + quantity
+}
+
+Movie.prototype.addStock = (quantity) => {
+    if (this.stock >= 0){
+        this.stock = this.stock - quantity
+    }
+}
 
 export default Movie
