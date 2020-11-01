63a4f10302318e25ae8c6964789db5b69926a53d
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-01 02:22:36 -0600

    feature: implement userSerializer in auth controllers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,20 +1,22 @@
 import userValidator from '../validators/user'
 import userService from '../services/user'
 import authService from '../services/auth'
+import userSerializer from '../serializers/userSerializer'
 
 const signIn = async (req, res) => {
+    // TODO: A logged can only create more accounts if is admin
     const { value , error } = userValidator.validate(req.body, {abortEarly: false})
+
+    let serializerFields = req.user?.isAdmin ? userSerializer.fullFields : userSerializer.defaultFields
 
     if(error){
         console.log("error", error.details)
         res.status(400).json({error: "error"})
     }
-
     try{
         console.log(value)
         const user = await userService.createUser(value)
-        console.log(user)
-        res.json(user)
+        res.json(userSerializer.genericSerializer(user, serializerFields))
     }catch (error) {
         console.log(error)
         res.status(400).json({error: error.message})
