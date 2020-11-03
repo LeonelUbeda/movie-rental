8e6acbb320c104b1042888baf45bb0231ba33c9c
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-02 19:01:11 -0600

    feature: permission middleware

---
+++ src/middlewares/permissionsMiddleware.js
@@ -0,0 +1,18 @@
+//This middleware checks if the user has permission to perform the requested action
+//And if user have permission, we assign in req.user.allowedFields the fields that user is allowed to
+export default function (entity, action) {
+    return function (req, res, next) {
+        if (
+            (Array.isArray(req.user.permissions[entity]?.[action]) && req.user.permissions[entity]?.[action].length > 0) ||
+            req.user.permissions[entity]?.[action] === true ||
+            typeof req.user.permissions[entity]?.[action] === 'string'
+        ){
+            req.user.allowedFields = req.user.permissions[entity][action]
+            next()
+        }else{
+            res.status(403).send()
+        }
+    }
+
+
+}
